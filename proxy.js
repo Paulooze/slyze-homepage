8ee5abcaeaf121d08aaf42ba78fb4c3000c75113
Matus Sabo <lamanabie@gmail.com>
2017-11-01 19:49:46 +0100

    CI deploy

--- proxy.js
+++ proxy.js
@@ -1,3 +1,5 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
 var httpProxy = require('http-proxy')
 var http = require('http')
 
@@ -8,6 +10,8 @@ var proxy = httpProxy.createProxyServer({
 // Creating new HTTP server.
 var server = http.createServer((req, res) => {
   console.log(req.headers)
+  console.log(req.url)
+
   if (req.url === '/') {
     req.url = '/index.html'
   }
