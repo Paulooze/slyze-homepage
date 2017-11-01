a237e84b30acc6057e974acd3410f268d220c475
Paulooze <paulooze@gmail.com>
2017-11-01 19:14:07 +0100

    build static

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mkdir ./static && cp -r ./dist/ ./static/dist/ && cp -r ./img/ ./static/img/ && cp ./android-chrome-96x96.png ./static/ && cp ./apple-touch-icon.png ./static/ && cp ./browserconfig.xml ./static/ && cp ./favicon-16x16.png ./static/ && cp ./favicon-32x32.png ./static/ && cp ./favicon.ico ./static/ && cp ./index.html ./static/ && cp ./manifest.json ./static/ && cp ./mstile-150x150.png ./static/
