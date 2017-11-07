2c0556ff9c9aaf644350ceec9988ad53aa702a86
Matus Sabo <lamanabie@gmail.com>
2017-11-07 19:37:38 +0100

    fix bin/cdn.sh

--- bin/cdn.sh
+++ bin/cdn.sh
@@ -1,4 +1,4 @@
 APPNAME="slyze-fe"
 BRANCH=$(git name-rev --name-only HEAD)
 
-gsutil rsync -d -r ./static gs://freshit-storage-public/$APPNAME-$BRANCH
+gsutil rsync -d -r -c ./static gs://freshit-storage-public/$APPNAME-$BRANCH
