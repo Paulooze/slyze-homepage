bddcb7e679b054e251c0ac38ebff79ed851f4888
paulooze <paulooze@gmail.com>
2018-11-01 19:33:40 +0100

    update otvaracie hodiny + novinka zvoz lyzi

--- Gulpfile.js
+++ Gulpfile.js
@@ -19,33 +19,40 @@ const environment = process.env.NODE_ENV === 'production' ? 'production' : 'deve
 console.log(`Building for environment ${environment}`);
 
 gulp.task('sass', () => {
-  const plugins = [
-    autoprefixer(),
-  ];
+  const plugins = [autoprefixer()];
   environment === 'production' && plugins.push(cssnano());
   gulp
-  .src('./src/scss/main.scss')
-  .pipe(sass({
-    errorLogToConsole: true,
-    outputStyle: 'expanded',
-  }).on('error', sass.logError))
-  .pipe(sourcemaps.init())
-  .pipe(postcss(plugins))
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest(`${baseDir}/css`))
+    .src('./src/scss/main.scss')
+    .pipe(
+      sass({
+        errorLogToConsole: true,
+        outputStyle: 'expanded',
+      }).on('error', sass.logError),
+    )
+    .pipe(sourcemaps.init())
+    .pipe(postcss(plugins))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(`${baseDir}/css`));
 });
 
 gulp.task('imagemin', () => {
-  gulp.src('./src/img/*')
-  .pipe(imagemin())
-  .pipe(webp())
-  .pipe(gulp.dest(`${baseDir}/img`));
+  gulp
+    .src('./src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest(`${baseDir}/img`));
+
+  gulp
+    .src('./src/img/*')
+    .pipe(webp())
+    .pipe(gulp.dest(`${baseDir}/img`));
 });
 
-gulp.task('webpack', () => gulp
-  .src('./src/js/main.js')
-  .pipe(gulpWebpack(Object.create(require('./webpack.config')), webpack))
-  .pipe(gulp.dest(`${baseDir}/js`)));
+gulp.task('webpack', () =>
+  gulp
+    .src('./src/js/main.js')
+    .pipe(gulpWebpack(Object.create(require('./webpack.config')), webpack))
+    .pipe(gulp.dest(`${baseDir}/js`)),
+);
 
 gulp.task('critical', () => {
   const criticalOpts = {
@@ -53,27 +60,34 @@ gulp.task('critical', () => {
     inline: true,
     css: [`${baseDir}/css/main.css`],
     include: ['.webp', '.no-webp'],
-    dimensions: [{
-      width: 400,
-      height: 700,
-    }, {
-      width: 768,
-      height: 1024,
-    }, {
-      width: 1024,
-      height: 768,
-    }, {
-      width: 1440,
-      height: 900,
-    }, {
-      width: 1920,
-      height: 1080,
-    }],
+    dimensions: [
+      {
+        width: 400,
+        height: 700,
+      },
+      {
+        width: 768,
+        height: 1024,
+      },
+      {
+        width: 1024,
+        height: 768,
+      },
+      {
+        width: 1440,
+        height: 900,
+      },
+      {
+        width: 1920,
+        height: 1080,
+      },
+    ],
   };
-  gulp.src('./index.html')
-  .pipe(critical({ ...criticalOpts }))
-  .on('error', err => gutil.log(gutil.colors.red(err.message)))
-  .pipe(gulp.dest('./static'));
+  gulp
+    .src('./index.html')
+    .pipe(critical({ ...criticalOpts }))
+    .on('error', err => gutil.log(gutil.colors.red(err.message)))
+    .pipe(gulp.dest('./static'));
 });
 
 gulp.task('serve', () => {
