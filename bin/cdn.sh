f23a52595e0c7f66cb50b575c901086386614d9a
Matus Sabo <edko@Matuss-MacBook-Pro.local>
2018-11-01 19:57:28 +0100

    fix cdn

--- bin/cdn.sh
+++ bin/cdn.sh
@@ -1,5 +1,4 @@
-APPNAME="slyze-fe"
-BRANCH=$(git name-rev --name-only HEAD)
+#!/bin/bash
 
-gsutil rm gs://freshit-storage-public/$APPNAME-$BRANCH/**
-gsutil -h "Cache-Control:private, max-age=0, no-transform" cp -Z -r static/** gs://freshit-storage-public/$APPNAME-$BRANCH/
+gsutil rm gs://slyze/**
+gsutil -h "Cache-Control:private, max-age=0, no-transform" cp -Z -r static/** gs://slyze
